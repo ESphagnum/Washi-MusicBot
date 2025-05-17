eedf207cffc54823e0369d91e7a5484bf814d0a2
ESphagnum <sfagnum14@gmail.com>
2025-05-17 20:19:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Music config.py
 import os
 
-language = os.getenv("LANG")
+language = os.getenv("Lang")
 
 SETTINGS = {
     "command_role": 1061998983158964285,
     "hasntRole_embed_color": 0xff1100,
-    "GUILD": os.getenv("GUILD"),
+    "GUILD": os.getenv("Guild"),
     "TOKEN": os.getenv("BOT_TOKEN")
 }
